bf8f5489be0a324bf1d6d372133addae89ff897e
Masnoon Nafees <masnoonnafees@Masnoons-MacBook-Pro.local>
2020-04-15 15:13:47 -0400

    Input Output Fixed

--- project/gan_latent.py
+++ project/gan_latent.py
@@ -14,12 +14,13 @@ from tensorflow.keras.losses import categorical_crossentropy, binary_crossentrop
 from tensorflow.keras.layers import Layer, Dense, Flatten, BatchNormalization, LeakyReLU, Dropout
 from matplotlib import pyplot
 from tensorflow.keras import backend as K
-from tensorflow.keras.optimizers import RMSprop
+from tensorflow.keras.optimizers import RMSprop, Adam
 from sklearn.model_selection import train_test_split
 from sklearn.neighbors import KDTree, NearestNeighbors
 from scipy.spatial.distance import pdist
 import os
-
+from tensorflow.keras.utils import plot_model
+from tensorflow.keras import Input
 from statistics import mean
 
 os.environ['KMP_DUPLICATE_LIB_OK'] = 'True'
@@ -83,7 +84,7 @@ def custom_loss_gan2(y_true, y2_pred):
 
 
 # define the standalone discriminator model
-def define_discriminator(n_inputs=1):
+def define_discriminator(input_shape_d, n_inputs=1):
     global g1_loss, g2_loss
     model = Sequential()
     # model.add(Flatten())
@@ -93,27 +94,33 @@ def define_discriminator(n_inputs=1):
     model.add(Dense(1, activation='linear'))
     # compile model
     opt = RMSprop(lr=0.0005)
+    validity = model(input_shape_d)
+    model = Model(input_shape_d, validity, name='Discriminator')
     model.compile(loss=wasserstein_loss, optimizer='adam', metrics=['accuracy'])
     return model
 
 
 # define the standalone generator model
-def define_generator(latent_dim, n_outputs=1):
+def define_generator(latent_dim, input_shape_g1, n_outputs=1):
     global score
     model = Sequential()
     # model.add(Flatten())
     # print(latent_dim)
     model.add(Dense(15, activation='relu', kernel_initializer='he_uniform', input_dim=latent_dim))
+    model.add(Dense(5, activation='relu', kernel_initializer='he_uniform', input_dim=latent_dim))
     # model.add(Dense(25, activation='relu', kernel_initializer='he_uniform', input_dim=latent_dim))
     #model.add(Flatten())
     model.add(Dense(n_outputs, activation='linear'))
+    #
+    gen_real = model(input_shape_g1)
+    model = Model(input_shape_g1, gen_real, name='Generator_1')
     model.compile(loss=wasserstein_loss, optimizer='adam', metrics=['accuracy'])
     return model
 
 
 ##Second generator
 
-def define_generator2(latent_dim, n_outputs=1):
+def define_generator2(latent_dim, input_shape_g2, n_outputs=1):
     global score
     model = Sequential()
     # model.add(Flatten())
@@ -122,30 +129,35 @@ def define_generator2(latent_dim, n_outputs=1):
     # model.add(Dense(25, activation='relu', kernel_initializer='he_uniform', input_dim=latent_dim))
     #model.add(Flatten())
     model.add(Dense(n_outputs, activation='linear'))
+    #model.compile(loss=wasserstein_loss, optimizer='adam', metrics=['accuracy'])
+    gen_fake = model(input_shape_g2)
+    model = Model(input_shape_g2, gen_fake, name='Generator_2')
     model.compile(loss=wasserstein_loss, optimizer='adam', metrics=['accuracy'])
     return model
 
 
-'''
+
 # define the combined generator and discriminator model, for updating the generator
-def define_gan(generator, generator2, discriminator):
+def define_gan(generator, generator2, discriminator, input_shape_g1, input_shape_g2, input_shape_d):
     global g1_loss, g2_loss
     # make weights in the discriminator not trainable
     #discriminator.trainable = False
     # connect them
-    model = Sequential()
-    # add generator
-    model.add(generator)
-    # add the second generator
-    model.add(generator2)
-    model.add(discriminator)
-    # compile model
-    # model.add(Flatten())
-    model.compile(loss=wasserstein_loss, optimizer='adam', metrics=['accuracy'])
-
-    model.summary()
+    generator.trainable = True
+    generator2.trainable = True
+    discriminator.trainable = False
+    #input_gen1 = Input(shape=input_shape_g)
+    #input_gen2 = Input(shape=input_shape_g)
+    d_input1 = generator(input_shape_g1)
+    d_input2 = generator2(input_shape_g2)
+    #d_input = np.concatenate([d_input1, d_input2])
+    valid_r = discriminator(d_input1)
+    valid_f = discriminator(d_input2)
+    model = Model(inputs=[input_shape_g1, input_shape_g2], outputs=[valid_r,valid_f], name='Combined_Advarsarial')
+    opt = Adam(lr=0.0002, beta_1=0.5)
+    model.compile(loss=[wasserstein_loss, wasserstein_loss], opttimizer = opt)
     return model
-'''
+
 
 
 def generate_real(generator, x):
@@ -252,12 +264,12 @@ def step(X, y, g_model, g_model2, d_model, num):
         #grads = [grad if grad is not None else tf.zeros_like(var)
         #        for var, grad in zip(model.trainable_variables, grads)]
         opt.apply_gradients(zip(grads, model.trainable_variables))
-        loss = tf.convert_to_tensor(l, np.float32)
+        #loss = tf.convert_to_tensor(l, np.float32)
         
 
 
 # train the generator and discriminator
-def train(g_model, g_model2, d_model, gan_model, batch_size, size, n_epochs=15, n_eval=2):
+def train(g_model, g_model2, d_model, gan_model, batch_size, size, n_epochs=100, n_eval=2):
     # determine half the size of one batch, for updating the discriminator
     # batch_size = 100
     batch_per_epoch = int(size / batch_size)
@@ -306,6 +318,10 @@ def train(g_model, g_model2, d_model, gan_model, batch_size, size, n_epochs=15, 
             score_each = d_model.predict(dx)
             score_concate = d_model.predict(dxx)
             score = np.mean(score_each)
+            x_gan_r = generate_latent_points(latent_dim, batch_size)
+            x_gan_f = generate_latent_points(latent_dim, batch_size)
+            x_gan_r = x_gan_r.reshape(-1, latent_dim)
+            x_gan_f = x_gan_f.reshape(-1, latent_dim)
             x_real_n = x_real_n.reshape(-1, latent_dim)
             #print('x_real_n', x_real_n.shape)
             y_real_n = y_real_n.reshape(-1, latent_dim)
@@ -314,27 +330,22 @@ def train(g_model, g_model2, d_model, gan_model, batch_size, size, n_epochs=15, 
             y_fake_n = y_fake_n.reshape(-1, latent_dim)
             n1 = n1.reshape(-1, latent_dim)
             n2 = n2.reshape(-1, latent_dim)
-            step(x_real_n, y_real_n, g_model, g_model2, d_model, 1)
-            step(x_fake_n, y_fake_n, g_model, g_model2, d_model, 2)
-            x_gan_r = generate_latent_points(latent_dim, batch_size)
-            x_gan_f = generate_latent_points(latent_dim, batch_size)
-            x_gan_r = x_gan_r.reshape(-1, latent_dim)
-            x_gan_f = x_gan_f.reshape(-1, latent_dim)
-            #g_model.train_on_batch(x_real_n,y_real_n)
-            #g_model.train_on_batch(x_fake_n, y_fake_n)
+            #step(x_real_n, y_real_n, g_model, g_model2, d_model, 1)
+            #step(x_fake_n, y_fake_n, g_model, g_model2, d_model, 2)
+            #g1_loss = g_model.train_on_batch(x_real_n,y_real_n)
+            #g2_loss = g_model2.train_on_batch(x_fake_n, y_fake_n)
             #g_model.train_on_batch(x_gan_r, y_real_n)
             #g_model2.train_on_batch(x_gan_r, y_real_n)
             #gan_model.train_on_batch(x_fake_n, y_fake_n)
-            g1_loss = g_model.evaluate(x_real_n, y_real_n, verbose=0)
-            g2_loss = g_model2.evaluate(x_fake_n, y_real_n, verbose=0)
-            
-            step(dxx, dyy, g_model, g_model2, d_model, 3)
-            dis_loss = d_model.evaluate(dxx, dyy)
+            #g2_loss = gan_model.evaluate([x_fake_n, y_real_n], []verbose=0)
+            #step(dxx, dyy, g_model, g_model2, d_model, 3)
+            dis_loss = d_model.train_on_batch(dxx, dyy)
+            gan_loss = gan_model.train_on_batch([x_gan_r, x_gan_f],[y_real_n, y_fake_n])
+            g1_loss = gan_loss[0]
+            g2_loss = gan_loss[1]
             f_nn.append(np.mean(n2))
             r_nn.append(np.mean(n1))
-
-            print("score", np.mean(score), "g1_loss", g1_loss, "g2_loss", g2_loss, "dis_loss", dis_loss)
-
+            print("score", np.mean(score), "g1_loss", g1_loss, "g2_loss", g2_loss, "dis_loss", dis_loss, "gan_loss", gan_loss)
             c1_hist.append(np.mean(dis_loss))
             g1_hist.append(np.mean(g1_loss))
             g2_hist.append(np.mean(g2_loss))
@@ -395,19 +406,19 @@ r_nn, f_nn = [], []
 count = 1
 count2 = 1
 # create the discriminator
-discriminator = define_discriminator()
+input_shape_d = Input(shape=(1,))
+input_shape_g1 = Input(shape=(latent_dim,))
+input_shape_g2 = Input(shape=(latent_dim,))
 # create the generator
-generator = define_generator(latent_dim)
-
-generator2 = define_generator2(latent_dim)
+generator = define_generator(latent_dim, input_shape_g1)
+generator2 = define_generator2(latent_dim, input_shape_g2)
+discriminator = define_discriminator(input_shape_d)
 # create the gan
-
-gan_model = 0
+gan_model = define_gan(generator, generator2, discriminator, input_shape_g1, input_shape_g2, input_shape_d)
 # train model
-# gan_model.summary()
+gan_model.summary()
 # plot gan model
-# plot_model(gan_model, to_file='gan_plot.png', show_shapes=True, show_layer_names=True)
-
+plot_model(gan_model, to_file='gan_plot.png', show_shapes=True, show_layer_names=True)
 train(generator, generator2, discriminator, gan_model, batch_size, size)
 print(c1_hist.count)
 print(len(c1_hist))
